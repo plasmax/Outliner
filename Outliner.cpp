e29da074f37f3a6fcc7569b7d152aee2d6bafad6
Max Last <tsalxam@gmail.com>
2018-09-23 19:25:40 +0100

    Working bi-directional selection with node_selected bool

--- Outliner.cpp
+++ Outliner.cpp
@@ -1,21 +1,5 @@
-
-// #define _SECURE_SCL 0
-
-// #include <string>
-// #include <sstream>
-
-// namespace patch
-// {
-//     template < typename T > std::string to_string( const T& n )
-//     {
-//         std::ostringstream stm ;
-//         stm << n ;
-//         return stm.str() ;
-//     }
-// }
-
-// #include <iostream>
-
+#include <iostream>
+#include <stdio.h>
 
 #include "DDImage/Op.h"
 #include "DDImage/Knob.h"
@@ -25,304 +9,305 @@
 #include "DDImage/gl.h"
 #include "DDImage/ViewerContext.h"
 #include "DDImage/TableKnobI.h"
+#include "DDImage/Matrix4.h"
 
 using namespace DD::Image;
 
 static const char* const CLASS = "Outliner";
-static const char* const HELP =
-"Filter and Transform Objects in a Scene. Selecting the object "
-"in the SceneView Knob selects it in the Scene, and vice-versa."
-"Additionally, object transformations can be parented in the SceneView"
-"And arbitrary transform handles can be added in the view."
-"And selecting multiple pieces of geometry allows you to move them all at once";
 
+// the goal (more or less)
+static const char* const HELP =
+"Highlight, Filter and Transform Objects in a Scene. Selecting the object "
+"in the Table_knob selects it in the Scene, and vice-versa."
+"And selecting multiple pieces of geometry allows you to move them all at once";
 
 
 class Outliner : public GeoOp {
 private:
-  bool _table_updated;
+  bool                       _table_initialized;
+  std::map<Hash, Matrix4>    _matrixlookup;
+  Matrix4                    _local;
+
+  static void draw_indicators_cb(void* p, ViewerContext* ctx) {
+    ((Outliner*)p)->draw_indicators(ctx);
+  };
 
 public:
   static const Op::Description description;
 
   Outliner(Node* node) : GeoOp(node) {
-    _table_updated = false;
+    _table_initialized = false;
+    _local.makeIdentity();
   }
 
   const char* Class() const { return CLASS; }
   const char* node_help() const { return HELP; }
+  const char* node_shape() const { return "O"; }
+  int minimum_inputs() const { return 1; }
+  int maximum_inputs() const { return -1; }
+  virtual bool inputs_clockwise() const { return false; }
 
-  void knobs(Knob_Callback f) {
-    //Handles data storage and mem management itself. Needs to have relevant
-    //cols added on creation only (hence f.makeKnobs check).
-    Knob* tableKnob = Table_knob(f, "Table_knob");
-    if (f.makeKnobs()) {
-      Table_KnobI* tableKnobI = tableKnob->tableKnob();
-      tableKnobI->addStringColumn("geo_names", "Geo Names", true, 170, false, true);
-      tableKnobI->addColumn("geo_hashes", "geo_hashes", Table_KnobI::StringColumn, true);
-    }
-  }
+  void knobs(Knob_Callback f);
+  int knob_changed(Knob* k);
 
-  void update_table() {
-    std::cout << "updating table" << std::endl;
-    GeometryList& out = *scene_->object_list();
+  void _validate(bool for_real);
+  void get_geometry_hash();
+  void geometry_engine(Scene& scene, GeometryList& out);
 
-    Knob* tableKnob = knob("Table_knob");
-    Table_KnobI* tk = tableKnob->tableKnob();
+  void build_handles(ViewerContext* ctx);
 
-    int numobjs = out.size();
-    std::cout << "out:" << numobjs << std::endl;
-    for (int r = 0; r < tk->getRowCount(); r++ ) {
-      tk->deleteRow(r);
-      }
-
-    for ( unsigned obj = 0; obj < out.size(); obj++ ) {
-      GeoInfo& info = out[obj];
-      const Hash hash = info.src_id();
-      tk->addRow(obj);
-      // std::string stringhash = patch::to_string(hash.value());
-      // tk->setCellString(obj, 1, stringhash);
-      }
-  }
-
-  void build_handles(ViewerContext* ctx) {
-    if (ctx->transform_mode() == VIEWER_2D) return;
-    if (ctx->viewer_mode() != VIEWER_2D && !node_disabled()) {
-      if (ctx->connected() >= SHOW_OBJECT) {
-        validate(false);
-
-        // construct output geometry and add callbacks to draw it in the viewer.
-        add_draw_geometry(ctx);
-
-        // ctx->connected(CONNECTED); // do not pass through objects
-        }
-      }
-
-    for (int i = 0; i <= inputs(); i++) {
-      if (!node_input(i, Op::OUTPUT_OP)) {
-        add_input_handle(i, ctx); // calls build_handles on input ops
-        }
-      }
-
-    build_knob_handles(ctx);
-
-    add_draw_handle(ctx);
-
-    if (!_table_updated) {
-      update_table();
-      _table_updated = true;
-      }
-
-    }
-
-
-  // Not used but potentially very useful
-  // (perhaps a more constant ID would be better)
-  void select_by_hash(Hash& hash) {
-    GeometryList& out = *scene_->object_list();
-    for (unsigned o = 0; o <= out.size(); o++) {
-      GeoInfo& info = out[o];
-      Hash obj_hash = info.src_id();
-      if (hash.value() == obj_hash.value()) {
-        info.selected = true; // my favourite line
-      }
-    }
-  }
-
-  void highlight() {
-    Knob* tableKnob = knob("Table_knob");
-    Table_KnobI* tk = tableKnob->tableKnob();
-    std::vector<int> selrows = tk->getSelectedRows();
-    std::cout << "selrows" << std::endl;
-    for (unsigned i = 0; i < selrows.size(); i++) {
-      std::cout << selrows[i] << std::endl;
-    }
-
-    GeometryList& out = *scene_->object_list();
-    for (unsigned o = 0; o <= out.size(); o++) {
-      GeoInfo& info = out[o];
-      if (!info.selectable) {
-        continue;
-      }
-
-      Hash hash = info.src_id();
-      for (unsigned i = 0; i < selrows.size(); i++) {
-        int sr = selrows[i];
-        std::cout << sr << std::endl;
-        if (sr == int(o)) {
-          info.selected = true; // my favourite line
-        } else { info.selected = false; }
-      }
-    }
-  }
-
-  void draw_handle(ViewerContext* ctx) {
-    GeoOp::draw_handle(ctx);
-
-    // if (ctx->event() == DRAW_OPAQUE)
-    // std::cout << "ctx.event: ";
-    // std::cout << ctx->event() << std::endl;
-
-    switch (ctx->event()) {
-      case NO_EVENT:
-        std::cout << "NO_EVENT, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-      case DRAW_OPAQUE:
-        std::cout << "DRAW_OPAQUE, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-      case DRAW_TRANSPARENT:
-        std::cout << "DRAW_TRANSPARENT, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-      case DRAW_STIPPLED:
-        std::cout << "DRAW_STIPPLED, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-      case DRAW_SHADOW:
-        std::cout << "DRAW_SHADOW, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-      case DRAW_LINES:
-        std::cout << "DRAW_LINES, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-      case MOVE:
-        std::cout << "MOVE, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-      case PUSH:
-        std::cout << "PUSH, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-      case DRAG:
-        std::cout << "DRAG, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-      case RELEASE:
-        std::cout << "RELEASE, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-      case KEY:
-        std::cout << "KEY, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-      case KEYUP:
-        std::cout << "KEYUP, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-      case DROP:
-        std::cout << "DROP, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-      case DROP_CHECK:
-        std::cout << "DROP_CHECK, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-      case ENTER_VIEWER:
-        std::cout << "ENTER_VIEWER, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-      case EXIT_VIEWER:
-        std::cout << "EXIT_VIEWER, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-      case CURSOR:
-        std::cout << "CURSOR, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-      case HOVER_ENTER:
-        std::cout << "HOVER_ENTER, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-      case HOVER_MOVE:
-        std::cout << "HOVER_MOVE, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-      case HOVER_LEAVE:
-        std::cout << "HOVER_LEAVE, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-      case DRAG_SELECT:
-        std::cout << "DRAG_SELECT, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-      case DRAG_SELECT_FINISHED:
-        std::cout << "DRAG_SELECT_FINISHED, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-      case FIRST_MENU:
-        std::cout << "FIRST_MENU, event index: ";
-        std::cout << ctx->event() << std::endl;
-        break;
-    }
-    if (   ctx->event() == PUSH
-        || ctx->event() == DRAG
-        || ctx->event() == RELEASE
-        || ctx->event() == DRAG_SELECT
-        || ctx->event() == DRAG_SELECT_FINISHED ) 
-    {
-      highlight();
-    }
-  }
-
-
-  int knob_changed(Knob* k) {
-    std::cout << k->name() << std::endl;
-    if (k->name() == "inputChange") {
-      _table_updated = false;
-      return 1;
-    }
-    if (k->name() == "showPanel") {
-      _table_updated = false;
-      return 1;
-    }
-    return 0;
-  }
-
-  bool handle(ViewerContext* ctx, int index) {
-    return true; // true means we are interested in the event
-  }
+  void init_table();
+  void select_geo_from_table(ViewerContext* ctx);
+  void select_table_from_geo(ViewerContext* ctx);
+  bool draw_indicators(ViewerContext* ctx);
 
 };
 
-/*
-NO_EVENT "only call menu(), otherwise ignore";
-DRAW_OPAQUE "solid objects/texture maps";
-DRAW_TRANSPARENT "transparent (below alpha threshold) 3D pass";
-DRAW_STIPPLED "draw things hidden behind solid objects";
-DRAW_SHADOW "shadows under lines in the 2D pass";
-DRAW_LINES "lines in 3D, and the entire 2D pass";
-MOVE "draw_handles() is being called to find tooltip";
-PUSH "user just pushed the mouse down, hit detection. Any handle callback which returns true for this event should also return true for DRAG and RELEASE";
-DRAG "user is dragging mouse, region selection hit detection. Any handle callback which returns true for this event should also return true for PUSH and RELEASE";
-RELEASE "user released the mouse. Any handle callback which returns true for this event should also return true for PUSH and DRAG";
-KEY "user hit a key";
-KEYUP "user let go of key";
-DROP "user dropped some data in a drag'n'drop operation";
-DROP_CHECK "user is dragging data over a handle";
-ENTER_VIEWER "the mouse has entered the viewer";
-EXIT_VIEWER "the mouse has exited the viewer";
-CURSOR "hit-detecting for a handle cursor";
-HOVER_ENTER "the hover was entered";
-HOVER_MOVE "user is hovering";
-HOVER_LEAVE "user has left hovering";
-DRAG_SELECT "user is drag-selecting handles";
-DRAG_SELECT_FINISHED "drag selection of handles has finished";
-FIRST_MENU "recommended start of Menu events";
-*/
 
-/*
-ViewerEvent {
-  NO_EVENT, DRAW_OPAQUE, DRAW_TRANSPARENT, DRAW_STIPPLED,
-  DRAW_SHADOW, DRAW_LINES, MOVE, PUSH,
-  DRAG, RELEASE, KEY, KEYUP,
-  DROP, DROP_CHECK, ENTER_VIEWER, EXIT_VIEWER,
-  CURSOR, HOVER_ENTER, HOVER_MOVE, HOVER_LEAVE,
-  DRAG_SELECT, DRAG_SELECT_FINISHED, FIRST_MENU
+void Outliner::knobs(Knob_Callback f) {
+  GeoOp::knobs(f);
+
+  Knob* tableKnob = Table_knob(f, "Table_knob");
+  if (f.makeKnobs()) {
+    Table_KnobI* tableKnobI = tableKnob->tableKnob();
+    tableKnobI->addColumn("src_id", "src_id", Table_KnobI::FloatColumn, false);
+  }
+
+  Axis_knob(f, &_local, "Axis_knob");
 }
-*/
+
+
+int Outliner::knob_changed(Knob* k) {
+  if (k->name() == "inputChange") {
+    _table_initialized = false;
+    return 1;
+  }
+  if (k->name() == "showPanel") {
+    _table_initialized = false;
+    return 1;
+  }
+  return 0;
+}
+
+
+void Outliner::_validate(bool for_real) {
+  update_geometry_hashes();
+  for (int i = 0; i < inputs(); i++) {
+    GeoOp* op = dynamic_cast<GeoOp*>(input(i));
+    if (op) { op->validate(for_real); }
+  }
+  GeoOp::_validate(for_real);
+}
+
+
+void Outliner::get_geometry_hash() {
+  GeoOp::get_geometry_hash();
+  geo_hash[Group_Points].append(Op::hash());
+
+  // the local transform
+  geo_hash[Group_Matrix].append(_local.a00);
+  geo_hash[Group_Matrix].append(_local.a01);
+  geo_hash[Group_Matrix].append(_local.a02);
+  geo_hash[Group_Matrix].append(_local.a03);
+
+  geo_hash[Group_Matrix].append(_local.a10);
+  geo_hash[Group_Matrix].append(_local.a11);
+  geo_hash[Group_Matrix].append(_local.a12);
+  geo_hash[Group_Matrix].append(_local.a13);
+
+  geo_hash[Group_Matrix].append(_local.a20);
+  geo_hash[Group_Matrix].append(_local.a21);
+  geo_hash[Group_Matrix].append(_local.a22);
+  geo_hash[Group_Matrix].append(_local.a23);
+
+  geo_hash[Group_Matrix].append(_local.a30);
+  geo_hash[Group_Matrix].append(_local.a31);
+  geo_hash[Group_Matrix].append(_local.a32);
+  geo_hash[Group_Matrix].append(_local.a33);
+
+  for (int i = 0; i <= inputs(); i++) {
+    if (node_input(i, Op::OUTPUT_OP) != NULL) {
+      GeoOp* op = input(i);
+      op->validate(true);
+      geo_hash[Group_Points].append(input(i)->hash(Group_Points));
+      geo_hash[Group_Primitives].append(input(i)->hash(Group_Primitives));
+      geo_hash[Group_Matrix].append(input(i)->hash(Group_Matrix));
+      geo_hash[Group_Attributes].append(input(i)->hash(Group_Attributes));
+    }
+  }
+}
+
+
+void Outliner::geometry_engine(Scene& scene, GeometryList& out) {
+
+  for (int i = 0; i <= inputs(); i++) {
+    if (dynamic_cast<GeoOp*>(input(i))){
+      GeoOp* op = input(i);
+      op->get_geometry(scene, out);
+    }
+  }
+
+  out.synchronize_objects();
+
+  // Apply transformations here (_local for now, but the goal is per object)
+  for (unsigned o = 0; o < out.size(); o++) {
+    GeoInfo& info = out[o];
+    info.matrix = _local * info.matrix;
+  }
+}
+
+
+/* fill the table with a row per GeoInfo in the scene.*/
+void Outliner::init_table() {
+  GeometryList& out = *scene_->object_list();
+
+  Knob* tableKnob = knob("Table_knob");
+  Table_KnobI* tk = tableKnob->tableKnob();
+
+  tk->suspendKnobChangedEvents();
+
+  // clear all items
+  while (tk->getRowCount()!=0) {
+    tk->deleteRow(0);
+  }
+
+  int src_column = tk->getColumnIndex("src_id");
+  std::vector<int> rows;
+
+  for ( unsigned o = 0; o < out.size(); o++ ) {
+    GeoInfo& info = out[o];
+    tk->addRow(o);
+
+    int src_id = int(info.src_id().value());
+    tk->setCellFloat(o, src_column, float(src_id));
+    if (!info.selectable) { continue; }
+    if (info.selected) {
+      rows.push_back(int(o));
+    }
+  }
+
+  // highlight currently selected geometry in table
+  tk->selectRows(rows);
+
+  tk->resumeKnobChangedEvents(true);
+}
+
+
+void Outliner::build_handles(ViewerContext* ctx) {
+
+  if (ctx->transform_mode() == VIEWER_2D) return;
+  if (ctx->viewer_mode() == VIEWER_2D) return;
+
+  if (ctx->viewer_mode() != VIEWER_2D && !node_disabled()) {
+    if (ctx->connected() >= SHOW_OBJECT) {
+      validate(false);
+
+      // construct output geometry and add callbacks to draw it in the viewer.
+      add_draw_geometry(ctx);
+
+      ctx->connected(CONNECTED); // do not pass through objects
+
+      ctx->add_draw_handle(draw_indicators_cb, this, node(), eDrawHandleAlways);
+      }
+    }
+
+  for (int i = 0; i <= inputs(); i++) {
+    if (!node_input(i, Op::OUTPUT_OP)) {
+      add_input_handle(i, ctx); // calls build_handles on input ops
+      }
+    }
+
+  build_knob_handles(ctx);
+
+  if (!_table_initialized) {
+    init_table();
+    _table_initialized = true;
+    }
+
+  // Don't draw anything unless viewer is in 3d mode:
+  if (ctx->transform_mode() == VIEWER_2D)
+    return;
+
+  // make it call draw_handle():
+  add_draw_handle(ctx);
+
+  }
+
+
+void Outliner::select_geo_from_table(ViewerContext* ctx) {
+
+  Knob* tableKnob = knob("Table_knob");
+  Table_KnobI* tk = tableKnob->tableKnob();
+  std::vector<int> selrows = tk->getSelectedRows();
+
+  GeometryList& out = *scene_->object_list();
+  for (unsigned o = 0; o <= out.size(); o++) {
+    GeoInfo& info = out[o];
+    if (!info.selectable) {
+      continue;
+    }
+
+    Hash hash = info.src_id();
+
+    bool selected_in_table = false;
+    for (unsigned ts = 0; ts < selrows.size(); ts++) {
+      int sr = selrows[ts];
+      if (sr == int(o)) {
+        selected_in_table = true;
+      }
+    }
+    info.selected = selected_in_table;
+  }
+
+}
+
+
+void Outliner::select_table_from_geo(ViewerContext* ctx) {
+  Knob* tableKnob = knob("Table_knob");
+  Table_KnobI* tk = tableKnob->tableKnob();
+
+  tk->suspendKnobChangedEvents();
+  int sel_column = tk->getColumnIndex("selected");
+
+  GeometryList& out = *scene_->object_list();
+
+  std::set<int> set;
+  std::vector<int> rows;
+  for (unsigned o = 0; o <= out.size(); o++) {
+    GeoInfo& info = out[o];
+    if (!info.selectable) { continue; }
+
+    tk->setCellBool(o, sel_column, info.selected);
+    if (info.selected) {
+      rows.push_back(int(o));
+      set.insert(int(o));
+    }
+  }
+  tk->selectRows(rows);
+  tk->resumeKnobChangedEvents(false);
+
+}
+
+
+bool Outliner::draw_indicators(ViewerContext* ctx) {
+  if (ctx->transform_mode() == VIEWER_2D) return false;
+  if (ctx->viewer_mode() == VIEWER_2D) return false;
+  if (display3d_ == DISPLAY_OFF) return false;
+
+  // looking for a better method tham node_selected to differentiate.
+  if (node_selected()) {
+    select_geo_from_table(ctx);
+  } else {
+    select_table_from_geo(ctx);
+  }
+
+  // just for fun, draw the current active bounding box. this actually seems to force it to update, giving a more accurate result when pressing 'f'
+  Box3& cbx = ctx->active_bbox();
+  glColor(2332084479);
+  gl_boxf(cbx.x(), cbx.y(), cbx.n(), cbx.r(), cbx.t(), cbx.f());
+
+  return false; // we're not interested in *every* event
+}
 
 
 static Op* build(Node *node) {
